cadc9e39c187c6263228e44a0ab7baeef839137d
yeongjae <yeongjae.blocket@gmail.com>
2022-11-23 00:01:58 +0900

    success to input data on model

--- board/views.py
+++ board/views.py
@@ -1,13 +1,20 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from board.models import PostDetail
 from board.models.post import Post
 
 
 def index(request):
+    print(1)
     posts = Post.objects.all()
     post_details = PostDetail.objects.all()
     post_all = {"posts": posts, "post_details_all": post_details}
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title)
+        PostDetail.objects.create(context=content)
     return render(request, './post_list.html', post_all)
+
 
